530d4094f94934151de4d9ff427614badde47ebf
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-04-20 15:40:41 +0100

    payment session model

---
+++ app/Models/PaymentSession.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentSession extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'booking_id',
+        'session_token',
+        'status',
+        'amount',
+    ];
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+}
