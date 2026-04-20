c640783cfc7eb2264600cc08568645d8d433de52
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-04-20 15:40:29 +0100

    booking model

---
+++ app/Models/Booking.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'space_id',
+        'start_time',
+        'end_time',
+        'status',
+        'payment_session_id',
+    ];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function space()
+    {
+        return $this->belongsTo(Space::class);
+    }
+
+    public function paymentSession()
+    {
+        return $this->belongsTo(PaymentSession::class);
+    }
+}
