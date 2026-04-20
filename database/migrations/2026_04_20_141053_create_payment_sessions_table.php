86dc4160046ace8b43518859266f67ac0caf08d3
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-04-20 15:40:06 +0100

    payment table migration

---
+++ database/migrations/2026_04_20_141053_create_payment_sessions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('booking_id')->nullable();
+            $table->string('session_token')->unique();
+            $table->enum('status', ['pending', 'paid', 'cancelled'])->default('pending');
+            $table->decimal('amount', 10, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_sessions');
+    }
+};
