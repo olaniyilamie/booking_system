b48cd95da4f2a860f81553d2f8cdb7152d3e0818
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-04-20 16:59:15 +0100

    formatted space json response

---
+++ app/Http/Resources/SpaceResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SpaceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'capacity' => (int) $this->capacity,
+            'hourly_rate' => (float) $this->hourly_rate,
+            'created_at' => $this->created_at?->toDateTimeString(),
+            'updated_at' => $this->updated_at?->toDateTimeString(),
+        ];
+    }
+}
