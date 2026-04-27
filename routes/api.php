deb56898392ecb23fdbd2d567170628b93f9928c
Olaniyi Olamide <olaniyilamie@gmail.com>
2026-04-27 12:09:42 +0100

    add booking routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BookingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +14,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BookingPaymentController;
 use App\Http\Controllers\API\SpaceController;
+
+
+// Bookings (authenticated)
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('spaces', [SpaceController::class, 'index']);
 Route::get('spaces/{space}', [SpaceController::class, 'show']);
+Route::prefix('bookings')->group(function () {
+    // Allow guest bookings via POST; other booking actions require auth
+    Route::post('/', [BookingController::class, 'store'])->middleware('throttle:booking-actions');
+
+    Route::middleware(['auth:sanctum', 'token.not.expired', 'throttle:auth-bookings'])->group(function () {
+        // Authenticated API routes
+        Route::get('/', [BookingController::class, 'index'])->name('bookings.index');
+        Route::get('/{booking}', [BookingController::class, 'show'])->name('bookings.show');
+        Route::get('/user/{user}', [BookingController::class, 'bookingsByUser']);
+        Route::get('/by-email', [BookingController::class, 'bookingsByEmail']);
+        Route::put('/{booking}', [BookingController::class, 'update']);
+        Route::delete('/{booking}', [BookingController::class, 'destroy']);
+
+        Route::post('/{booking}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel.api')->middleware('throttle:booking-actions');
+        Route::post('/{booking}/pay', [BookingPaymentController::class, 'pay']);
+    });
+});
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
